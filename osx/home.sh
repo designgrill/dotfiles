49e8eec99c82afc9ae83c2feffa320c477ceb379
Anshul Agrawal <anshul@designgrill.com>
2014-01-25 20:57:32 +0530

    Other minor changes

--- osx/home.sh
+++ osx/home.sh
@@ -1,10 +1,10 @@
-mv ~/.ssh ~/.sshOLD
+#mv ~/.ssh ~/.sshOLD
 ln -s ~/Dropbox/Settings/.ssh ~/.ssh
 chmod -R og-rxw ~/.ssh
 mv ~/.bash_profile ~/.bash_profileOLD
 mv ~/.bash_history ~/.bash_historyOLD
-ln -s ~/Dropbox/Settings/.bash_profile ~/.bash_profile
-ln -s ~/Dropbox/Settings/.bash_history ~/.bash_history
+ln -s ~/Dropbox/Settings/mygola-meenal/.bash_profile ~/.bash_profile
+ln -s ~/Dropbox/Settings/mygola-meenal/.bash_history ~/.bash_history
 
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
@@ -12,7 +12,7 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
-mv ~/.vimrc ~/.vimrcOLD
+#mv ~/.vimrc ~/.vimrcOLD
 ln -s ~/Dropbox/Settings/.vimrc ~/.vimrc
-mv ~/.subversion ~/.subversionOLD
-ln -s ~/Dropbox/Settings/.subversion ~/.subversion
+#mv ~/.subversion ~/.subversionOLD
+#ln -s ~/Dropbox/Settings/.subversion ~/.subversion
