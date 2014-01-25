cc8df3c2ee28039686358fe8e6323086d6315860
Anshul Agrawal <anshul@designgrill.com>
2014-01-25 20:57:47 +0530

    Sublime preference changes

--- osx/sublime.sh
+++ osx/sublime.sh
@@ -2,6 +2,6 @@ rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ De
 curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(dark).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Dark\).tmTheme
 curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(light).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Light\).tmTheme
 mv ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settingsOLD
-ln -s ~/Dropbox/Settings/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+ln -s ~/Documents/desktop-settings/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 
 sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /bin/subl
