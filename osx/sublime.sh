ef83879949505f7c27491d8baf0b141085079abf
Anshul Agrawal <anshul@designgrill.com>
2014-04-25 10:42:35 +0530

    Upgraded to Sublime 3

--- osx/sublime.sh
+++ osx/sublime.sh
@@ -1,7 +1,7 @@
-rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/Solarized\ \(*\).tmTheme.cache
-curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(dark).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Dark\).tmTheme
-curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(light).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Light\).tmTheme
-mv ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settingsOLD
-ln -s ~/Documents/desktop-settings/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(*\).tmTheme.cache
+curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(dark).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Dark\).tmTheme
+curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(light).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Light\).tmTheme
+mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settingsOLD
+ln -s ~/Documents/desktop-settings/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
-sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /bin/subl
+sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /bin/subl
