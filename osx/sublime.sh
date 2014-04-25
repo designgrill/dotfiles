9e2297d39a14372f12f3e686d24300a7dc6670ed
Anshul Agrawal <anshul@designgrill.com>
2014-04-25 23:10:03 +0530

    Mkdir for sublime color themes

--- osx/sublime.sh
+++ osx/sublime.sh
@@ -1,4 +1,5 @@
 rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(*\).tmTheme.cache
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/
 curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(dark).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Dark\).tmTheme
 curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(light).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Light\).tmTheme
 mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settingsOLD
