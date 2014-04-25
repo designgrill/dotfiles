14834df28522743320818d3f316013204fa1d0ae
Anshul Agrawal <anshul@designgrill.com>
2014-04-25 23:31:12 +0530

    Changing the Solarized source

--- osx/sublime.sh
+++ osx/sublime.sh
@@ -1,7 +1,8 @@
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(*\).tmTheme.cache
+rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(*\).tmTheme
+rm ~/Library/Application\ Support/Sublime\ Text\ 3/Cache/Color\ Scheme\ -\ Default/Solarized\ \(*\).tmTheme.cache
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/
-curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(dark).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Dark\).tmTheme
-curl -L "https://github.com/altercation/solarized/raw/master/textmate-colors-solarized/Solarized%20(light).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Light\).tmTheme
+curl -L "https://github.com/deplorableword/textmate-solarized/raw/master/Solarized%20(dark).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Dark\).tmTheme
+curl -L "https://github.com/deplorableword/textmate-solarized/raw/master/Solarized%20(light).tmTheme" -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Color\ Scheme\ -\ Default/Solarized\ \(Light\).tmTheme
 mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settingsOLD
 ln -s ~/Documents/desktop-settings/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
