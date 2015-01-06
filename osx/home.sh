12c4734c72010b1f8ced22692798c944f5f932b0
Anshul Agrawal <anshul@designgrill.com>
2015-01-06 20:58:11 +0530

    Follow redirects for github pathogen

--- osx/home.sh
+++ osx/home.sh
@@ -8,7 +8,7 @@ ln -s ~/Dropbox/Settings/mygola-meenal/.bash_history ~/.bash_history
 
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-curl -Sso ~/.vim/autoload/pathogen.vim \
+curl -LSso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
