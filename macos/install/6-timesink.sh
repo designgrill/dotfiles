f47a2202712451c174962893e4e700eabadf8f0c
Anshul Agrawal <anshul@designgrill.com>
2020-03-31 20:29:21 +0530

    Removed imagemagick

--- macos/install/6-timesink.sh
+++ macos/install/6-timesink.sh
@@ -7,14 +7,6 @@ else
     brew install "ffmpeg"
 fi
 
-
-# imagemagick
-if brew list -1 | grep -q "imagemagick"; then
-    brew upgrade "imagemagick"
-else
-    brew install "imagemagick"
-fi
-
 # Xcode
 mas install 497799835 # Xcode 497799835
 echo "⛳️ Checking if you need to accept Xcode license. Password will be asked to accept."
