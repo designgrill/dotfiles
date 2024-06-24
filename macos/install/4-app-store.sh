f336b4b8ce70a1da408fcf5148386875235655c5
Anshul Agrawal <anshul@designgrill.com>
2024-06-24 16:22:36 +0530

    Updated Mac Apps

--- macos/install/4-app-store.sh
+++ macos/install/4-app-store.sh
@@ -10,8 +10,6 @@ mas install 417602904 # CloudApp 417602904
 mas install 406056744 # Evernote 406056744
 mas install 405399194 # Kindle 405399194
 mas install 1274495053 # Microsoft ToDo
-mas install 880663569 # Pixlr 880663569
 mas install 803453959 # Slack 803453959
-mas install 485812721 # TweetDeck 485812721
-mas install 409789998 # Twitter 409789998
+mas install 1448916662 # Step Two 1448916662
 mas install 1147396723 # WhatsApp 1147396723
