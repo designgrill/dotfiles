f2b3778be174526112b3ba86a60d3b94633b5d6b
Anshul Agrawal <anshul@designgrill.com>
2025-11-02 22:20:59 +0530

    Moved to Python3 for simple http server

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -6,7 +6,7 @@ alias ip="ifconfig -a | perl -nle'/((\d+\.){3}\d+)/ && print \$1'"
 alias large="find ./ -type f -size +5M -print0 | xargs -0 ls -halt | sort -rn -k5"
 alias ls="ls --color=auto"
 alias reload='source ~/.bashrc'
-alias serve='python -m SimpleHTTPServer'
+alias serve='python3 -m http.server'
 alias socks='ssh -fND 8888'
 alias gitg='open -a SourceTree "$(git rev-parse --show-toplevel)"'
 alias groot='cd "$(git rev-parse --show-toplevel)"'
